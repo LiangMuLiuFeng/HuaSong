224230c8b83a2ee377584028e676d1f76221cb76
枫 <78782154+LiangMuLiuFeng@users.noreply.github.com>
2022-02-21 18:25:03 +0800

    compile
    java-run

---
+++ compile/Java.bat
@@ -0,0 +1,12 @@
+@echo off
+echo.
+echo Run the Java Program
+echo.
+echo       — Made by : Wang Zijia
+echo.
+echo.
+echo Please Input name of program which you want:
+set /p name=">>> "
+cls
+java %name%
+pause>nul
