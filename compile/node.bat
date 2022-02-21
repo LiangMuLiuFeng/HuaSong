0984c2d74767e89cffa871e835ad16e073db2720
枫 <78782154+LiangMuLiuFeng@users.noreply.github.com>
2022-02-21 18:22:25 +0800

    compile
    node-compile

---
+++ compile/node.bat
@@ -0,0 +1,12 @@
+@echo off
+echo.
+echo Run the Node Program
+echo.
+echo       — Made by : Wang Zijia
+echo.
+echo.
+echo Please Input name of program which you want:
+set /p name=">>> "
+cls
+node %name%
+pause>nul
